5abe5d00836e3dea6f3648eda41e28453bf33edc
D1M30 <113018363+D1M30@users.noreply.github.com>
2022-12-08 15:45:22 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,295 @@
+//  ========================================================
+//  ====================Домашка №3=================================
+
+// Вывести в консоль 1-5
+
+// 1)
+
+// let num = 1;
+// while (num < 6){
+//    console.log(num);
+//    num++;
+// }
+
+// --------------
+// 2)
+
+// let num = 1;
+// do{
+//    console.log(num);
+//    num++;
+// } while(num < 6)
+
+// --------------
+// 3)
+
+// for(let num = 1; num < 6; num++){
+//    console.log(num);
+// }
+
+// --------------
+// 4)
+
+// for (let num = 1; num < 9; num++){
+//    console.log(num);
+//    if (num == 5) break;
+// }
+
+// --------------
+// 5)
+
+// for ( num = 0; num < 6; num++){
+//    if (num == 0) continue;
+//    console.log(num);
+// }
+
+// --------------------------------------------------------
+
+                     // Задача №3
+
+// let num = 0;
+// while(num < 3){
+//    console.log(num)
+//    num++
+// }
+
+                       // Задача №4
+
+// for (let num = 0; num < 2; num++) {
+
+//    for (let size = 0; size < 3; size++){
+//       console.log(size)
+//       if (size == 1) break; 
+//    }
+// }
+
+
+//   ========================================================
+//  ====================Домашка №4 (number)=================================
+
+               // Задача №1
+
+// let numOne = (1.005 * 100) / 100
+// console.log(numOne)
+
+
+
+                // Задача №2
+
+   // let value = `135.58px`;
+   // console.log(parseFloat (value))
+
+
+
+
+                 // Задача №3
+   
+   // let value = 58 + `Фрилансер`;
+   // if (isNaN(value)){
+
+   //    console.log(`Результат выражения NaN`)
+   // }
+
+
+
+
+                  // Задача №4
+   
+   // console.log(Math.max(10, 58, 39, -150, 0));
+
+
+
+
+                  // Задача №5
+
+   // console.log(Math.floor(58.858));
+
+
+
+
+
+//   ========================================================
+//  ====================Домашка №5 (string)=================================
+
+             // Задача №2
+
+// let text =  `фрилансер`;
+// console.log(text[5])
+
+
+
+               // Задача №4
+
+// let text =  `фрилансер`;
+// console.log(text.toUpperCase())
+
+
+
+
+                  // Задача №5
+
+//  let text =  `фрилансер`;
+//  console.log(text.slice(3,6))
+
+
+
+
+                  // Задача №5
+
+ let text =  `фрилансер`;
+ console.log(text.slice(3,6))
+
+
+
+
+
+
+
+
+
+
+// ПРАКТИКА
+
+
+
+
+//====================================
+// Длинна ряда
+
+// let count = `Hello world!`;
+// console.log(count.length)
+
+//====================================
+//Находим элемент в масиве
+
+// let count = `@Hello world!`;
+// console.log(count[0]);
+
+//====================================
+//Upper/lowerCase
+
+// let count = `BIG small`;
+// console.log(count.toUpperCase());
+// console.log(count.toLowerCase());
+
+//====================================
+// Поиск
+
+
+// let count = `HeLlo woRld!`;
+// console.log(count.includes(`r`))
+
+// let countLow = count.toLowerCase();
+// console.log(countLow.includes(`rld`))
+
+//====================================
+// часть ряда
+
+// let  someString = `123456789`
+// someNewString = someString.slice(2 , 5)
+
+// console.log(someNewString)
+
+//====================================
+// Поиск и изменения
+
+// let frase = `@Good morning!`;
+// frase = frase.replace(`@Good`, ``);
+
+// console.log(frase)
+
+//====================================
+// Округление
+
+// let num = 36.45;
+
+
+// console.log(num[2 ])
+
+// console.log(Math.floor(num));
+// console.log(Math.ceil(num));
+// console.log(Math.round(num));
+// console.log(Math.random());
+
+//====================================
+
+// let someVar = ` 123.9`;
+// console.log(someVar);
+// let newS = parseFloat(someVar);
+
+// console.log(newS);
+
+//====================================
+// Операторы
+
+// let varOne = `5`;
+// let varTwo = `5`;
+// let result = +varOne + +varTwo;
+// console.log(result)
+
+// 
+// let newS = !``;
+
+// console.log(Boolean (newS));
+// console.log(typeof newS);
+
+// let someVar = 0 || 0;
+// console.log(someVar)
+
+
+// let someVar = 555  && 5;
+
+// console.log(someVar)
+// console.log(Boolean (someVar))
+
+
+// let varOne = 2 
+// let vatTwo = 7;
+
+// if(varOne >= 1){
+//    console.log(`varOne is true`)
+// }
+
+
+// let varOne = 1;
+// let varTwo = 3;
+// let varThree = 3;
+// let varFour = 6;
+
+// if(varOne > 3 || varTwo===2 && varThree < 4 || varFour >= 6){
+
+// console.log(`Условие true`)
+// }
+
+
+
+// console.log(someString[0])
+// console.log(someString[1])
+// console.log(someString[2])
+// console.log(someString[3])
+// console.log(someString[4])
+// console.log(someString[5])
+
+// let someString = `Hello!`;
+// for (let i =0; i < someString.length; ++i) {
+//    console.log(someString[i]);
+// }
+
+// let samOne = 6
+// let samTwo = 5
+
+// let result = samOne > samTwo ? samOne + samTwo : samOne * samTwo;
+
+// console.log(result)
+
+// if (samOne > samTwo){
+//    console.log(samOne + samTwo)
+// }else{
+//    console.log(samOne * samTwo)
+// }
+
+
+
+
+
+
